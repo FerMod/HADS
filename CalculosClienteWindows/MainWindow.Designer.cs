12b02c2bf87a86569a9f6b3efc0170316b0dda04
FerMod <tudelaigarcia@gmail.com>
2019-02-02 17:17:42 +0100

    Change target framework version
    Tweaked window style

--- CalculosClienteWindows/MainWindow.Designer.cs
+++ CalculosClienteWindows/MainWindow.Designer.cs
@@ -28,10 +28,10 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
-			this.tbTotalSinIva = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
-			this.tbIva = new System.Windows.Forms.TextBox();
 			this.mtbTotal = new System.Windows.Forms.TextBox();
+			this.lTotalSinIva = new System.Windows.Forms.Label();
+			this.lIva = new System.Windows.Forms.Label();
 			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
 			this.bCalcular = new System.Windows.Forms.Button();
 			this.tableLayoutPanel1.SuspendLayout();
@@ -40,41 +40,41 @@
 			// 
 			// tableLayoutPanel1
 			// 
-			this.tableLayoutPanel1.AutoSize = true;
 			this.tableLayoutPanel1.ColumnCount = 3;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
 			this.tableLayoutPanel1.Controls.Add(this.cbTipoIva, 1, 1);
 			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
 			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 3);
-			this.tableLayoutPanel1.Controls.Add(this.tbTotalSinIva, 1, 3);
 			this.tableLayoutPanel1.Controls.Add(this.label4, 0, 4);
-			this.tableLayoutPanel1.Controls.Add(this.tbIva, 1, 4);
 			this.tableLayoutPanel1.Controls.Add(this.mtbTotal, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.lTotalSinIva, 1, 3);
+			this.tableLayoutPanel1.Controls.Add(this.lIva, 1, 4);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(10, 10, 10, 10);
+			this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(10);
 			this.tableLayoutPanel1.RowCount = 6;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(287, 209);
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(344, 209);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// cbTipoIva
 			// 
-			this.cbTipoIva.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.cbTipoIva.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbTipoIva.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.cbTipoIva.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.cbTipoIva.FormattingEnabled = true;
-			this.cbTipoIva.Location = new System.Drawing.Point(132, 52);
+			this.cbTipoIva.Location = new System.Drawing.Point(132, 43);
 			this.cbTipoIva.Name = "cbTipoIva";
-			this.cbTipoIva.Size = new System.Drawing.Size(125, 26);
+			this.cbTipoIva.Size = new System.Drawing.Size(179, 26);
 			this.cbTipoIva.TabIndex = 2;
 			// 
 			// label1
@@ -82,7 +82,7 @@
 			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(13, 19);
+			this.label1.Location = new System.Drawing.Point(13, 16);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(113, 18);
 			this.label1.TabIndex = 0;
@@ -93,7 +93,7 @@
 			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(13, 56);
+			this.label2.Location = new System.Drawing.Point(13, 47);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(113, 18);
 			this.label2.TabIndex = 0;
@@ -104,69 +104,74 @@
 			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label3.Location = new System.Drawing.Point(13, 113);
+			this.label3.Location = new System.Drawing.Point(13, 94);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(113, 18);
 			this.label3.TabIndex = 0;
 			this.label3.Text = "Total sin IVA:";
-			// 
-			// tbTotalSinIva
-			// 
-			this.tbTotalSinIva.Anchor = System.Windows.Forms.AnchorStyles.Left;
-			this.tbTotalSinIva.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.tbTotalSinIva.Location = new System.Drawing.Point(132, 110);
-			this.tbTotalSinIva.Name = "tbTotalSinIva";
-			this.tbTotalSinIva.ReadOnly = true;
-			this.tbTotalSinIva.Size = new System.Drawing.Size(125, 24);
-			this.tbTotalSinIva.TabIndex = 3;
 			// 
 			// label4
 			// 
 			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(13, 150);
+			this.label4.Location = new System.Drawing.Point(13, 117);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(113, 18);
 			this.label4.TabIndex = 0;
 			this.label4.Text = "IVA:";
 			// 
-			// tbIva
-			// 
-			this.tbIva.Anchor = System.Windows.Forms.AnchorStyles.Left;
-			this.tbIva.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.tbIva.Location = new System.Drawing.Point(132, 147);
-			this.tbIva.Name = "tbIva";
-			this.tbIva.ReadOnly = true;
-			this.tbIva.Size = new System.Drawing.Size(125, 24);
-			this.tbIva.TabIndex = 4;
-			// 
 			// mtbTotal
 			// 
+			this.mtbTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.mtbTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.mtbTotal.Location = new System.Drawing.Point(132, 13);
 			this.mtbTotal.Name = "mtbTotal";
-			this.mtbTotal.Size = new System.Drawing.Size(125, 24);
+			this.mtbTotal.Size = new System.Drawing.Size(179, 24);
 			this.mtbTotal.TabIndex = 1;
+			// 
+			// lTotalSinIva
+			// 
+			this.lTotalSinIva.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.lTotalSinIva.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lTotalSinIva.Location = new System.Drawing.Point(132, 92);
+			this.lTotalSinIva.Name = "lTotalSinIva";
+			this.lTotalSinIva.Size = new System.Drawing.Size(179, 23);
+			this.lTotalSinIva.TabIndex = 0;
+			this.lTotalSinIva.Text = "0.0";
+			this.lTotalSinIva.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// lIva
+			// 
+			this.lIva.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.lIva.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lIva.Location = new System.Drawing.Point(132, 115);
+			this.lIva.Name = "lIva";
+			this.lIva.Size = new System.Drawing.Size(179, 23);
+			this.lIva.TabIndex = 0;
+			this.lIva.Text = "0.0";
+			this.lIva.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// flowLayoutPanel1
 			// 
 			this.flowLayoutPanel1.AutoSize = true;
 			this.flowLayoutPanel1.Controls.Add(this.bCalcular);
 			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
 			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 209);
 			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(287, 40);
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(344, 40);
 			this.flowLayoutPanel1.TabIndex = 1;
 			// 
 			// bCalcular
 			// 
-			this.bCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.bCalcular.Location = new System.Drawing.Point(5, 5);
-			this.bCalcular.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+			this.bCalcular.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.bCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.bCalcular.Location = new System.Drawing.Point(239, 5);
+			this.bCalcular.Margin = new System.Windows.Forms.Padding(5);
 			this.bCalcular.Name = "bCalcular";
 			this.bCalcular.Size = new System.Drawing.Size(100, 30);
-			this.bCalcular.TabIndex = 5;
+			this.bCalcular.TabIndex = 3;
 			this.bCalcular.Text = "Calcular";
 			this.bCalcular.UseVisualStyleBackColor = true;
 			this.bCalcular.Click += new System.EventHandler(this.BCalcular_Click);
@@ -176,7 +181,7 @@
 			this.AcceptButton = this.bCalcular;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(287, 249);
+			this.ClientSize = new System.Drawing.Size(344, 249);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Controls.Add(this.flowLayoutPanel1);
 			this.Name = "MainWindow";
@@ -201,8 +206,8 @@
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 		private System.Windows.Forms.Button bCalcular;
-		private System.Windows.Forms.TextBox tbTotalSinIva;
-		private System.Windows.Forms.TextBox tbIva;
 		private System.Windows.Forms.TextBox mtbTotal;
+		private System.Windows.Forms.Label lTotalSinIva;
+		private System.Windows.Forms.Label lIva;
 	}
 }
--- CalculosClienteWindows/MainWindow.cs
+++ CalculosClienteWindows/MainWindow.cs
@@ -1,31 +1,37 @@
-﻿using CalculosDinero;
-using CalculosDinero.Iva;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.Globalization;
 using System.Windows.Forms;
+using CalculosDinero;
+using CalculosDinero.Iva;
 
 namespace CalculosClienteWindows {
 
 	public partial class MainWindow : Form {
 
+		private RegionInfo regionInfo;
+
 		public MainWindow() {
 			InitializeComponent();
+
+			regionInfo = new RegionInfo(System.Threading.Thread.CurrentThread.CurrentUICulture.LCID);
+
+			lTotalSinIva.Text = $"0.00 {regionInfo.CurrencySymbol}";
+			lIva.Text = $"0.00 {regionInfo.CurrencySymbol}";
+
 		}
+
+		#region Components Initialization
+
+		protected void InitDropDownItems() {
+			cbTipoIva.DataSource = Enum.GetValues(typeof(TipoIva));
+		}
+
+		#endregion
+
+		#region Components Events
 
 		private void Application_Load(object sender, EventArgs e) {
 			InitDropDownItems();
-		}
-
-		protected void InitDropDownItems() {
-
-			cbTipoIva.DataSource = Enum.GetValues(typeof(TipoIva));
-
 		}
 
 		private void BCalcular_Click(object sender, EventArgs e) {
@@ -35,14 +41,13 @@ namespace CalculosClienteWindows {
 				bool isNumeric = double.TryParse(mtbTotal.Text, out double precioTotal);
 				if(isNumeric) {
 
-					TipoIva tipoIva;
-					Enum.TryParse<TipoIva>(cbTipoIva.SelectedValue.ToString(), out tipoIva);
+					Enum.TryParse(cbTipoIva.SelectedValue.ToString(), out TipoIva tipoIva);
 
 					double precioBruto = Math.Round(CalculoIva.ObtenerPrecioBruto(precioTotal, tipoIva), 2);
 					double precioIva = Math.Round(CalculoIva.ObtenerPrecioIva(precioTotal, tipoIva), 2);
 
-					tbTotalSinIva.Text = $"{precioBruto:0.00}";
-					tbIva.Text = $"{precioIva:0.00}";
+					lTotalSinIva.Text = $"{precioBruto:0.00} {regionInfo.CurrencySymbol}";
+					lIva.Text = $"{precioIva:0.00} {regionInfo.CurrencySymbol}";
 
 				}
 
@@ -50,6 +55,7 @@ namespace CalculosClienteWindows {
 
 		}
 
+		#endregion
 	}
 
 }
