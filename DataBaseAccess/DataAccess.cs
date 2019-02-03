611e0992a80128915054cf3495a6b93d43ff2a3b
FerMod <tudelaigarcia@gmail.com>
2019-02-03 17:38:08 +0100

    Add data base access library

--- DataBaseAccess/DataAccess.cs
+++ DataBaseAccess/DataAccess.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataBaseAccess {
 
 	public class DataAccess {
+
+		public Dictionary<long, Factura> ListaFacturas { get; set; } = new Dictionary<long, Factura>();
+
 	}
 
 }
