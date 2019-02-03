c30497827fb4635ccd9f6bd798ac5986ed71645e
FerMod <tudelaigarcia@gmail.com>
2019-02-03 15:26:43 +0100

    Add DataBaseAccess library and unit tests

---
+++ Test/TestCalculosLib.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using CalculosDinero;
+using CalculosDinero.Iva;
+using NUnit.Framework;
+
+namespace Tests {
+
+	[TestFixture]
+	public class TestCalculosLib {
+
+		private static double Tolerance => 0.01d;
+
+		private static IEnumerable<TestCaseData> PrecioBrutoTestData {
+			get {
+				yield return new TestCaseData(1000, TipoIva.General, 826.45);
+				yield return new TestCaseData(1000, TipoIva.Reducido, 909.09);
+				yield return new TestCaseData(1000, TipoIva.SuperReducido, 961.54);
+				yield return new TestCaseData(1000, TipoIva.Ninguno, 1000.0);
+			}
+		}
+
+		private static IEnumerable<TestCaseData> PrecioIvaTestData {
+			get {
+				yield return new TestCaseData(1000, TipoIva.General, 173.55);
+				yield return new TestCaseData(1000, TipoIva.Reducido, 90.91);
+				yield return new TestCaseData(1000, TipoIva.SuperReducido, 38.46);
+				yield return new TestCaseData(1000, TipoIva.Ninguno, 0.0);
+			}
+		}
+
+		[Test, Description("Precio sin IVA"), TestCaseSource(nameof(PrecioBrutoTestData))]
+		public void TestPrecioBruto(double total, TipoIva tipoIva, double expected) {
+			double value = CalculoIva.ObtenerPrecioBruto(total, tipoIva);
+			Assert.That(expected, Is.EqualTo(value).Within(Tolerance));
+		}
+
+		[Test, Description("Precio IVA"), TestCaseSource(nameof(PrecioIvaTestData))]
+		public void TestPrecioIva(double total, TipoIva tipoIva, double expected) {
+			double value = CalculoIva.ObtenerPrecioIva(total, tipoIva);
+			Assert.That(expected, Is.EqualTo(value).Within(Tolerance));
+		}
+
+	}
+
+}
---
+++ Test/TestDataAccess.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace Tests {
+
+	public class TestDataAccess {
+
+		[SetUp]
+		public void SetUp() {
+		}
+
+		[TearDown]
+		public void TearDown() {
+		}
+
+		[Test, Description("")]
+		public void Test1() {
+			Assert.Pass();
+		}
+
+	}
+
+}
