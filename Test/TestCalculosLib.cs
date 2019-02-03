67ba287fca18e178b3ebe23ccb722ef585194a24
FerMod <tudelaigarcia@gmail.com>
2019-02-03 17:39:52 +0100

    Add unit tests for 'CalculosLib' and 'DataBaseAccess' libraries

--- Test/TestCalculosLib.cs
+++ Test/TestCalculosLib.cs
@@ -4,10 +4,10 @@ using CalculosLib;
 using CalculosLib.Iva;
 using NUnit.Framework;
 
-namespace Tests {
+namespace Tests.CalculosLib {
 
 	[TestFixture]
-	public class TestCalculosLib {
+	public class TestCalculosIva {
 
 		private static double Tolerance => 0.01d;
 
@@ -17,6 +17,16 @@ namespace Tests {
 				yield return new TestCaseData(1000, TipoIva.Reducido, 909.09);
 				yield return new TestCaseData(1000, TipoIva.SuperReducido, 961.54);
 				yield return new TestCaseData(1000, TipoIva.Ninguno, 1000.0);
+
+				yield return new TestCaseData(0.0, TipoIva.General, 0.0);
+				yield return new TestCaseData(0.0, TipoIva.Reducido, 0.0);
+				yield return new TestCaseData(0.0, TipoIva.SuperReducido, 0.0);
+				yield return new TestCaseData(0.0, TipoIva.Ninguno, 0.0);
+
+				yield return new TestCaseData(-1000, TipoIva.General, -826.45);
+				yield return new TestCaseData(-1000, TipoIva.Reducido, -909.09);
+				yield return new TestCaseData(-1000, TipoIva.SuperReducido, -961.54);
+				yield return new TestCaseData(-1000, TipoIva.Ninguno, -1000.0);
 			}
 		}
 
@@ -26,10 +36,20 @@ namespace Tests {
 				yield return new TestCaseData(1000, TipoIva.Reducido, 90.91);
 				yield return new TestCaseData(1000, TipoIva.SuperReducido, 38.46);
 				yield return new TestCaseData(1000, TipoIva.Ninguno, 0.0);
+
+				yield return new TestCaseData(0.0, TipoIva.General, 0.0);
+				yield return new TestCaseData(0.0, TipoIva.Reducido, 0.0);
+				yield return new TestCaseData(0.0, TipoIva.SuperReducido, 0.0);
+				yield return new TestCaseData(0.0, TipoIva.Ninguno, 0.0);
+
+				yield return new TestCaseData(-1000, TipoIva.General, -173.55);
+				yield return new TestCaseData(-1000, TipoIva.Reducido, -90.91);
+				yield return new TestCaseData(-1000, TipoIva.SuperReducido, -38.46);
+				yield return new TestCaseData(-1000, TipoIva.Ninguno, -0.0);
 			}
 		}
 
-		[Test, Description("Precio sin IVA"), TestCaseSource(nameof(PrecioBrutoTestData))]
+		[Test, Description("Precio Bruto"), TestCaseSource(nameof(PrecioBrutoTestData))]
 		public void TestPrecioBruto(double total, TipoIva tipoIva, double expected) {
 			double value = CalculoIva.ObtenerPrecioBruto(total, tipoIva);
 			Assert.That(expected, Is.EqualTo(value).Within(Tolerance));
@@ -39,6 +59,43 @@ namespace Tests {
 		public void TestPrecioIva(double total, TipoIva tipoIva, double expected) {
 			double value = CalculoIva.ObtenerPrecioIva(total, tipoIva);
 			Assert.That(expected, Is.EqualTo(value).Within(Tolerance));
+		}
+
+	}
+
+	[TestFixture]
+	public class TestTipoIva {
+
+		class TipoIvaDataClass {
+
+			public static IEnumerable<TestCaseData> GetValueTestCases {
+				get {
+					yield return new TestCaseData(TipoIva.General).Returns(0.21);
+					yield return new TestCaseData(TipoIva.Reducido).Returns(0.10);
+					yield return new TestCaseData(TipoIva.SuperReducido).Returns(0.04);
+					yield return new TestCaseData(TipoIva.Ninguno).Returns(0.0);
+				}
+			}
+
+			public static IEnumerable<TestCaseData> GetNameTestCases {
+				get {
+					yield return new TestCaseData(TipoIva.General).Returns("General");
+					yield return new TestCaseData(TipoIva.Reducido).Returns("Reducido");
+					yield return new TestCaseData(TipoIva.SuperReducido).Returns("SuperReducido");
+					yield return new TestCaseData(TipoIva.Ninguno).Returns("Ninguno");
+				}
+			}
+
+		}
+
+		[Test, Description("GetValue"), TestCaseSource(typeof(TipoIvaDataClass), "GetValueTestCases")]
+		public double TestGetValue(TipoIva tipoIva) {
+			return tipoIva.GetValue();
+		}
+
+		[Test, Description("GetName"), TestCaseSource(typeof(TipoIvaDataClass), "GetNameTestCases")]
+		public string TestGetName(TipoIva tipoIva) {
+			return tipoIva.GetName();
 		}
 
 	}
--- Test/TestDataAccess.cs
+++ Test/TestDataAccess.cs
@@ -1,20 +1,62 @@
+using System.Collections.Generic;
+using CalculosLib.Iva;
+using DataBaseAccess;
 using NUnit.Framework;
 
-namespace Tests {
+namespace Tests.DataBaseAccess {
 
 	public class TestDataAccess {
 
+		private DataAccess dataAccess;
+
+		private static IEnumerable<TestCaseData> FacturaTestData {
+			get {
+				yield return new TestCaseData(1, 1000, TipoIva.General);
+				yield return new TestCaseData(2, 1000, TipoIva.Reducido);
+				yield return new TestCaseData(3, 1000, TipoIva.SuperReducido);
+				yield return new TestCaseData(4, 1000, TipoIva.Ninguno);
+
+				yield return new TestCaseData(5, 0, TipoIva.General);
+				yield return new TestCaseData(6, 0, TipoIva.Reducido);
+				yield return new TestCaseData(7, 0, TipoIva.SuperReducido);
+				yield return new TestCaseData(8, 0, TipoIva.Ninguno);
+
+				yield return new TestCaseData(9, -1000, TipoIva.General);
+				yield return new TestCaseData(10, -1000, TipoIva.Reducido);
+				yield return new TestCaseData(11, -1000, TipoIva.SuperReducido);
+				yield return new TestCaseData(12, -1000, TipoIva.Ninguno);
+			}
+		}
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp() {
+			dataAccess = new DataAccess();
+		}
+
 		[SetUp]
 		public void SetUp() {
+			dataAccess.ListaFacturas.Clear();
 		}
 
-		[TearDown]
-		public void TearDown() {
+		[Test, Description("Add Factura"), TestCaseSource(nameof(FacturaTestData))]
+		public void TestAddFactura(int id, double total, TipoIva tipoIva) {
+			Factura factura = new Factura(id, total, tipoIva);
+			Assert.That(dataAccess.ListaFacturas.TryAdd(id, factura));
 		}
 
-		[Test, Description("")]
-		public void Test1() {
-			Assert.Pass();
+		[Test, Description("Remove Factura"), TestCaseSource(nameof(FacturaTestData))]
+		public void TestRemoveFactura(int id, double total, TipoIva tipoIva) {
+			Factura factura = new Factura(id, total, tipoIva);
+			Assume.That(dataAccess.ListaFacturas.TryAdd(id, factura));
+			Assert.That(dataAccess.ListaFacturas.Remove(factura.Id));
+		}
+
+		[Test, Description("Get Factura"), TestCaseSource(nameof(FacturaTestData))]
+		public void TestGetFactura(int id, double total, TipoIva tipoIva) {
+			Factura expected = new Factura(id, total, tipoIva);
+			Assume.That(dataAccess.ListaFacturas.TryAdd(id, expected));
+			Assert.That(dataAccess.ListaFacturas.TryGetValue(id, out Factura factura));
+			Assert.That(expected, Is.EqualTo(factura));
 		}
 
 	}
