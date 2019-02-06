60be727430b8983c5ffc3df67d1cb1a3eb7651a8
FerMod <tudelaigarcia@gmail.com>
2019-02-06 12:02:50 +0100

    Add validation field

--- CalculosClienteWeb/Global.asax.cs
+++ CalculosClienteWeb/Global.asax.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Web.UI;
 
-namespace CalculosClienteWeb
-{
-    public class Global : System.Web.HttpApplication
-    {
-        protected void Application_Start(object sender, EventArgs e)
-        {
-        }
-    }
+namespace CalculosClienteWeb {
+
+	public class Global : System.Web.HttpApplication {
+
+		protected void Application_Start(object sender, EventArgs e) {
+
+			ScriptManager.ScriptResourceMapping.AddDefinition("jquery", new ScriptResourceDefinition {
+				Path = "~/scripts/jquery-3.3.1.min.js",
+				DebugPath = "~/scripts/jquery-3.3.1.min.js",
+				CdnPath = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js",
+				CdnDebugPath = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.js"
+			});
+
+		}
+
+	}
+
 }
